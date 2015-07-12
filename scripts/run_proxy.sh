2d406c947ab073273d3b5a6a1c620dc381b62cca
Johan Vromans <jvromans@squirrel.nl>
2015-07-12 22:26:15 +0200

    (run) Re-use logfile.

--- scripts/run_proxy.sh
+++ scripts/run_proxy.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
 lib=$HOME/wrk/Growatt
+logfile=`date "+%Y%m%d%H%M%S.log"`
 
 while [ a = a ]
 do
-    perl $lib/scripts/growatt_proxy.pl --debug > `date "+%Y%m%d%H%M%S.log"` 2>&1 || sleep 60
+    perl $lib/scripts/growatt_proxy.pl --debug >>$logfile  2>&1
+    if [ $? != 0 ]
+    then
+	sleep 60
+	# logfile=`date "+%Y%m%d%H%M%S.log"`
+    fi
 done
