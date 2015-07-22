54ae1d2c2441940e8d2329c17a310cda0549a421
Johan Vromans <jvromans@squirrel.nl>
2015-07-22 20:23:27 +0200

    Use logfile per day.

--- scripts/run_proxy.sh
+++ scripts/run_proxy.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 lib=$HOME/wrk/Growatt
-logfile=`date "+%Y%m%d%H%M%S.log"`
 
 while [ a = a ]
 do
+    logfile=`date "+%Y%m%d.log"`
     perl $lib/scripts/growatt_proxy.pl --debug >>$logfile  2>&1
     if [ $? != 0 ]
     then
 	sleep 60
-	# logfile=`date "+%Y%m%d%H%M%S.log"`
     fi
 done
