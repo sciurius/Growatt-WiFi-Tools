f1272316c4dcf3be960e70b17102e815c64bd89c
Johan Vromans <jvromans@squirrel.nl>
2016-09-08 14:40:05 +0200

    Update for new server programs.

--- scripts/run_proxy.sh
+++ scripts/run_proxy.sh
@@ -5,7 +5,8 @@ lib=$HOME/wrk/Growatt
 while [ a = a ]
 do
     logfile=`date "+%Y%m%d.log"`
-    perl $lib/scripts/growatt_proxy.pl --debug >>$logfile  2>&1
+    # Remove --remote for standalone server.
+    perl $lib/scripts/growatt_server.pl --debug --remote >>$logfile  2>&1
     if [ $? != 0 ]
     then
 	sleep 60
